b8cae006bd5f70ef8c7df0595576cc28a1a6256c
andresbn <andresbn21@hotmail.com>
2014-10-23 00:40:44 -0500

    Los 4 test válidos del mantenimiento Curso Implementados (faltan ajustes menores)

--- MatriculaCore/src/test/java/pe/com/core/excel/Excel.java
+++ MatriculaCore/src/test/java/pe/com/core/excel/Excel.java
@@ -15,8 +15,7 @@ public class Excel {
         List<List<String>> lista;
         lista = new ArrayList<List<String>>();
         try{
-            FileInputStream archivo = new FileInputStream(
-                    new File("C:\\ProgramasInstalados\\DatosParaMatricula.xls"));
+            FileInputStream archivo = new FileInputStream(new File("C:\\Documents and Settings\\pruebas\\My Documents\\NetBeansProjects\\MatriculaPruebas\\Documentos\\DatosParaMatricula.xls"));
             //Archivo en excel
             HSSFWorkbook archivoExcel = new HSSFWorkbook(archivo);
             //Con que hoja de excel voy a trabajar
@@ -48,16 +47,15 @@ public class Excel {
         return lista;
     }
     
-    public List<List<String[]>> leer_ModificarCurso(){
-        List<List<String[]>> lista;
-        lista = new ArrayList<List<String[]>>();
+    public List<List<String>> leer_ModificarCurso(){
+        List<List<String>> lista;
+        lista = new ArrayList<List<String>>();
         try{
-            FileInputStream archivo = new FileInputStream(
-                    new File("C:\\ProgramasInstalados\\DatosParaMatricula.xls"));
+            FileInputStream archivo = new FileInputStream(new File("C:\\Documents and Settings\\pruebas\\My Documents\\NetBeansProjects\\MatriculaPruebas\\Documentos\\DatosParaMatricula.xls"));
             //Archivo en excel
             HSSFWorkbook archivoExcel = new HSSFWorkbook(archivo);
             //Con que hoja de excel voy a trabajar
-            HSSFSheet hojaExcel = archivoExcel.getSheetAt(0);
+            HSSFSheet hojaExcel = archivoExcel.getSheetAt(1);
             //Obtener las filas
             Iterator<Row> filas = hojaExcel.iterator();
             //Pasar a leer la segunda fila
@@ -65,15 +63,15 @@ public class Excel {
             
             while(filas.hasNext()){
                 int cont=0;
-                List<String[]> listaInterna= new ArrayList<String[]>();
+                List<String> listaInterna= new ArrayList<String>();
                 Row fila = filas.next();
                 Iterator<Cell> celdas = fila.cellIterator();
                 while(celdas.hasNext()){
                     Cell celda = celdas.next();
                     if(cont==2 || cont==4){
-                        listaInterna.add(new String[]{String.valueOf(celda.getNumericCellValue())});
+                        listaInterna.add(new String(String.valueOf((int)celda.getNumericCellValue())));
                     }else{
-                        listaInterna.add(new String[]{celda.getStringCellValue()});
+                        listaInterna.add(new String(celda.getStringCellValue()));
                     }
                     cont++;                        
                 }
--- MatriculaCore/src/test/java/pe/com/core/test/CursoTest.java
+++ MatriculaCore/src/test/java/pe/com/core/test/CursoTest.java
@@ -25,7 +25,7 @@ public class CursoTest {
     }
     
     @Test
-    public void CrarCurso_Valido(){
+    public void CrearCurso_Valido(){
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
 	CursoDAO cursoDAO = context.getBean(CursoDAO.class);
         
@@ -53,4 +53,61 @@ public class CursoTest {
         }
     }
     
+    @Test
+    public void ModificarCurso_Valido(){
+        
+        //Leer de excel la data
+        List<List<String>> data = new Excel().leer_ModificarCurso();
+        for(List<String> listainterna : data){
+            selenium.open("/MatriculaWeb/ModificarCurso.jsp");
+            selenium.click("linkModificarCurso");
+            selenium.waitForPageToLoad("30000");
+            selenium.type("txtNombre", listainterna.get(0));
+            selenium.type("txtCodigo", listainterna.get(1));
+            selenium.type("txtCreditos", listainterna.get(2));
+            selenium.type("txtRequisitos", listainterna.get(3));
+            selenium.type("txtCiclo", listainterna.get(4));
+            selenium.click("btnModificarCurso");
+            selenium.waitForPageToLoad("30000");
+            
+            Assert.assertEquals(selenium.getText("lblMensaje"), "El curso ha sido modificado correctamente.");
+        }
+    }
+    
+    @Test
+    public void EliminarCurso_Valido(){
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+	CursoDAO cursoDAO = context.getBean(CursoDAO.class);
+        
+        int cantidadDeCursosAlInicio,cantidadDeCursosAlFinal;
+        
+        List<Curso> lista;
+        lista = new ArrayList<Curso>();
+        lista=cursoDAO.list();
+        cantidadDeCursosAlInicio = lista.size();
+                
+        selenium.open("/MatriculaWeb/EliminarCurso.jsp");
+        selenium.click("linkEliminarCurso");
+        selenium.waitForPageToLoad("30000");
+
+        cantidadDeCursosAlFinal = cursoDAO.list().size();
+        Assert.assertTrue(cantidadDeCursosAlFinal<cantidadDeCursosAlInicio);
+        
+    }
+    
+    @Test
+    public void ListarCurso_Valido(){
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+	CursoDAO cursoDAO = context.getBean(CursoDAO.class);
+        
+        selenium.open("/MatriculaWeb/ListarCurso.jsp");
+        selenium.waitForPageToLoad("30000");
+        
+        List<Curso> lista;
+//        lista = new ArrayList<Curso>();
+        lista=cursoDAO.list();
+
+        Assert.assertTrue(lista!=null);
+        
+    }
 }
