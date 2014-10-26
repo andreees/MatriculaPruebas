2d3cd1eca4d3854bbd73c0d6270d982ae56a0b4d
cemp2703 <cemp2703@gmail.com>
2014-10-25 23:02:12 -0500

    s

---
+++ MatriculaDB - solo data.sql
@@ -0,0 +1,8 @@
+INSERT INTO `usuario` VALUES (1,'hwong','027e4180beedb29744413a7ea6b84a42'),(2,'rtaza','d4c285227493531d0577140a1ed03964'),(3,'epalomino','6d6354ece40846bf7fca65dfabd5d9d4'),(4,'abaquerizo','231badb19b93e44f47da1bd64a8147f2'),(5,'cpalma','6f597c1ddab467f7bf5498aad1b41899');
+
+INSERT INTO `administrador` VALUES (1,'Henry','Wong',1),(2,'Eduardo','Palomino',3);
+
+INSERT INTO `alumno` VALUES (1,'Roy','Taza',2),(2,'Andres','Baquerizo',4),(3,'Cesar','Palma',5);
+
+
+
